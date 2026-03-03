8f7bc25b30f9b0ece3dabe3be940d81d94d60cf7
Faith <213283810+faith-20-code@users.noreply.github.com>
2026-03-03 14:57:28 +0300

    l have creted the models, views, admin.py and urls. So far the system is working well and doing what it should do but there is further improvements to be done. l added a group model because l couldnt connect the work, user and subtask without it but its not difficult, it actually amkes the work simpler and easier to understand.

---
+++ Backend/management/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("group/create/", GroupCreateView.as_view(), name="group_create"),
+    path("work/create/", WorkCreateView.as_view(), name="work_create"),
+    path("subtask/create/", SubTaskCreateView.as_view(), name="subtask_create"),
+]
